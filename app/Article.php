b2db46ae058c48dbd37ff6c8a568db616f858f09
Ganjar Setia <ganjar_setia@ymail.com>
2017-10-11 00:41:22 +0700

    Add article
    Signed-off-by: Ganjar Setia <ganjar_setia@ymail.com>

---
+++ app/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    
+    protected $table = "articles";
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+        'user_id','title','slug','excerpts','body',
+    ];
+    
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+}
